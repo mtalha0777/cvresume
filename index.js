2a40eec50c4c0056b1360acc07141c2d3b889b32
Muhammad Talha <125496925+mtalha0777@users.noreply.github.com>
2023-03-22 13:13:52 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express=require("express");
-const app=express();
+const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3001;
 
-const indexrouters=require("./router/routers");
-app.use("/", indexrouters);
+const userroute = require('./routes/router');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/user', userroute)
+
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+    console.log(`Running At http://${hostname}:${port}/`);
+})
+
+
